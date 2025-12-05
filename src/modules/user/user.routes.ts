51ed6265e673ca6ce121bddfda0717b9f634da82
nurulamin092 <mnurulamin092@gmail.com>
2025-12-05 07:10:24 +0600

    jwt auth

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { userController } from "./user.controller";
 import logger from "../../middleware/logger";
+import auth from "../../middleware/auth";
 
 const router = express.Router();
 
 router.post("/", userController.createUser);
 
-router.get("/", logger, userController.getUser);
+router.get("/", logger, auth(), userController.getUser);
 
 router.get("/:id", userController.getSingleUser);
 router.put("/:id", userController.updatedUser);
